c8975746b685144b7eda4abed085906b1dc7490f
Awe Joseph <awejoseph96@gmail.com>
2022-09-13 16:38:30 -0700

    commmiting fileof 4put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,13 +2,11 @@
 
 /**
  * main - Entry point
- * 
- * Return: Always 0 (Success)
- * 
+ *
+ * Return: Always 0 (Sucess)
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-i}
+}
