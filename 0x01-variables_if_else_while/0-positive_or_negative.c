80c1ba170ca9a7cdd29ee1ccb20b510aa44b405d
Awe Joseph <awejoseph96@gmail.com>
2022-09-21 08:41:57 -0700

    comit fule

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,4 +1,4 @@
-nclude <stdlib.h>
+#include <stdlib.h>
 #include <time.h>
 /* more headers goes there */
 
@@ -6,6 +6,7 @@ nclude <stdlib.h>
 int main(void)
 {
 	int n;
+
 	srand(time(0));	
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
