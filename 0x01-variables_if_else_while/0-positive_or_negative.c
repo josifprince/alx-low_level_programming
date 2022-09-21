964f4978bbec196607be2c369fef850c3c71cb2e
Awe Joseph <awejoseph96@gmail.com>
2022-09-21 09:07:08 -0700

    commiting a file

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,7 +7,6 @@
 int main(void)
 {
 	int n;
-
 	srand(time(0));	
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
@@ -23,6 +22,5 @@ int main(void)
 	{	
 		printf("%d is negative\n", n);
 	}
-
 	return (0);
 }
